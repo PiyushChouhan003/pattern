3129a0948d11aafea354f919f49aa9e9b7be06f4
PiyushChouhan003 <chouhanpiyush003@gmail.com>
2023-07-04 17:12:38 +0530

    first commit

---
+++ EghthProblem.java
@@ -0,0 +1,21 @@
+public class EghthProblem {
+    
+
+    public static void main (String[] args){
+        int n = 5;
+for (int i=1; i<=n; i++){
+    for(int j=1; j<=i; j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+int a = 4;
+       for (int i=a; i>=1; i--){
+    for(int j=1; j<=i; j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+    }
+    
+}
